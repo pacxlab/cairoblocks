c84c8ba7c224f13e6b43560b6b10f67fe9a81d00
Ibrahim Suleiman Prince <suleimanonnet@gmail.com>
2024-08-14 23:55:44 +0100

    chore: update header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,17 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="title">CairoBlocks</div>
+      <div className="title">
+        CairoBlocks{" "}
+        <span>
+          <img src="" />
+        </span>
+      </div>
+      <div className="middle-btns">
+        <button className="">Run</button>
+        <button className="">Stop</button>
+      </div>
+      <div className="reset-btn">hello</div>
     </div>
   );
 };

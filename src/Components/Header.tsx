7c9fea4c3e33fec6ea1d1610c270f5af6969b636
Ibrahim Suleiman Prince <suleimanonnet@gmail.com>
2024-08-15 01:58:27 +0100

    chore: update header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,19 +5,36 @@ import RefreshSVG from "./refresh";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="logo">
-        <div>CairoBlocks</div>
-        <img height="60px" width="60px" src={BlockImg} />
+    <>
+      {" "}
+      <div className="header">
+        <div className="logo">
+          <div>CairoBlocks</div>
+          <img height="60px" width="60px" src={BlockImg} />
+        </div>
+        <div className="middle-btns">
+          <button className="run">Run</button>
+          <button className="stop">Stop</button>
+        </div>
+        <div className="refresh-btn">
+          <RefreshSVG />
+        </div>
       </div>
-      <div className="middle-btns">
-        <button className="run">Run</button>
-        <button className="stop">Stop</button>
+      <div className="sub-header">
+        <div className="ctrl-btns">
+          <div className="view-ctrl">View</div>
+          <div className="ctrl-btns-inner">
+            <button>BLOCK VIEW</button>
+            <button>TEXT VIEW</button>
+          </div>
+          <input type="range" style={{ backgroundColor: "red" }} />
+        </div>
+        <div className="mode-btns">
+          <div className="code">CODE</div>
+          <div className="decode">DECODE</div>
+        </div>
       </div>
-      <div className="refresh-btn">
-        <RefreshSVG />
-      </div>
-    </div>
+    </>
   );
 };
 

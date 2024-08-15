b482d358d3cd25a5664f9f51bc8ea5ad33a33fad
Ibrahim Suleiman Prince <suleimanonnet@gmail.com>
2024-08-15 01:07:05 +0100

    chore: add header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "./Header.css";
+import BlockImg from "../block-img.png";
+import RefreshSVG from "./refresh";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="title">
-        CairoBlocks{" "}
-        <span>
-          <img src="" />
-        </span>
+      <div className="logo">
+        <div>CairoBlocks</div>
+        <img height="60px" width="60px" src={BlockImg} />
       </div>
       <div className="middle-btns">
-        <button className="">Run</button>
-        <button className="">Stop</button>
+        <button className="run">Run</button>
+        <button className="stop">Stop</button>
       </div>
-      <div className="reset-btn">hello</div>
+      <div className="refresh-btn">
+        <RefreshSVG />
+      </div>
     </div>
   );
 };

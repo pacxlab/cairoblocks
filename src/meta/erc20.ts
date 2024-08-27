3dd0be7450f3e45f60010b276c5c68b444e05d55
CyndieKamau <cyndiekamaa@gmail.com>
2024-08-27 19:42:15 +0300

    feat: add erc20 ink smart contract

--- src/meta/erc20.ts
+++ src/meta/erc20.ts
@@ -1,209 +1,154 @@
-const erc20: string = `#[starknet::interface]
-pub trait IERC20<TContractState> {
-    fn get_name(self: @TContractState) -> felt252;
-    fn get_symbol(self: @TContractState) -> felt252;
-    fn get_decimals(self: @TContractState) -> u8;
-    fn get_total_supply(self: @TContractState) -> felt252;
-    fn balance_of(self: @TContractState, account: ContractAddress) -> felt252;
-    fn allowance(
-        self: @TContractState, owner: ContractAddress, spender: ContractAddress
-    ) -> felt252;
-    fn transfer(ref self: TContractState, recipient: ContractAddress, amount: felt252);
-    fn transfer_from(
-        ref self: TContractState,
-        sender: ContractAddress,
-        recipient: ContractAddress,
-        amount: felt252
-    );
-    fn approve(ref self: TContractState, spender: ContractAddress, amount: felt252);
-    fn increase_allowance(ref self: TContractState, spender: ContractAddress, added_value: felt252);
-    fn decrease_allowance(
-        ref self: TContractState, spender: ContractAddress, subtracted_value: felt252
-    );
-}
+const erc20: string = `#![cfg_attr(not(feature = "std"), no_std)]
 
-#[starknet::contract]
-pub mod erc20 {
-    use core::num::traits::Zero;
-    use starknet::get_caller_address;
-    use starknet::contract_address_const;
-    use starknet::ContractAddress;
+use ink_lang as ink;
 
-    #[storage]
-    struct Storage {
-        name: felt252,
-        symbol: felt252,
-        decimals: u8,
-        total_supply: felt252,
-        balances: LegacyMap::<ContractAddress, felt252>,
-        allowances: LegacyMap::<(ContractAddress, ContractAddress), felt252>,
+#[ink::contract]
+mod erc20 {
+    #[cfg(not(feature = "ink-as-dependency"))]
+    use ink_storage::{
+        collections::HashMap as StorageHashMap,
+        lazy::Lazy,
+    };
+
+    /// A simple ERC-20 contract.
+    #[ink(storage)]
+    pub struct Erc20 {
+        /// Total token supply.
+        total_supply: Lazy<Balance>,
+        /// Mapping from owner to number of owned token.
+        balances: StorageHashMap<AccountId, Balance>,
+        /// Mapping of the token amount which an account is allowed to withdraw
+        /// from another account.
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>,
     }
 
-    #[event]
-    #[derive(Copy, Drop, Debug, PartialEq, starknet::Event)]
-    pub enum Event {
-        Transfer: Transfer,
-        Approval: Approval,
-    }
-    #[derive(Copy, Drop, Debug, PartialEq, starknet::Event)]
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
     pub struct Transfer {
-        pub from: ContractAddress,
-        pub to: ContractAddress,
-        pub value: felt252,
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
     }
-    #[derive(Copy, Drop, Debug, PartialEq, starknet::Event)]
+
+    /// Event emitted when an approval occurs that spender is allowed to withdraw
+    /// up to the amount of value tokens from owner.
+    #[ink(event)]
     pub struct Approval {
-        pub owner: ContractAddress,
-        pub spender: ContractAddress,
-        pub value: felt252,
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
     }
 
-    mod Errors {
-        pub const APPROVE_FROM_ZERO: felt252 = 'ERC20: approve from 0';
-        pub const APPROVE_TO_ZERO: felt252 = 'ERC20: approve to 0';
-        pub const TRANSFER_FROM_ZERO: felt252 = 'ERC20: transfer from 0';
-        pub const TRANSFER_TO_ZERO: felt252 = 'ERC20: transfer to 0';
-        pub const BURN_FROM_ZERO: felt252 = 'ERC20: burn from 0';
-        pub const MINT_TO_ZERO: felt252 = 'ERC20: mint to 0';
+    /// The ERC-20 error types.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Returned if not enough balance to fulfill a request is available.
+        InsufficientBalance,
+        /// Returned if not enough allowance to fulfill a request is available.
+        InsufficientAllowance,
     }
 
-    #[constructor]
-    fn constructor(
-        ref self: ContractState,
-        recipient: ContractAddress,
-        name: felt252,
-        decimals: u8,
-        initial_supply: felt252,
-        symbol: felt252
-    ) {
-        self.name.write(name);
-        self.symbol.write(symbol);
-        self.decimals.write(decimals);
-        self.mint(recipient, initial_supply);
-    }
+    /// The ERC-20 result type.
+    pub type Result<T> = core::result::Result<T, Error>;
 
-    #[abi(embed_v0)]
-    impl IERC20Impl of super::IERC20<ContractState> {
-        fn get_name(self: @ContractState) -> felt252 {
-            self.name.read()
+    impl Erc20 {
+        /// Creates a new ERC-20 contract with the specified initial supply.
+        #[ink(constructor)]
+        pub fn new(initial_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = StorageHashMap::new();
+            balances.insert(caller, initial_supply);
+            let instance = Self {
+                total_supply: Lazy::new(initial_supply),
+                balances,
+                allowances: StorageHashMap::new(),
+            };
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: initial_supply,
+            });
+            instance
         }
 
-        fn get_symbol(self: @ContractState) -> felt252 {
-            self.symbol.read()
+        /// Returns the total token supply.
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            *self.total_supply
         }
 
-        fn get_decimals(self: @ContractState) -> u8 {
-            self.decimals.read()
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balances.get(&owner).copied().unwrap_or(0)
         }
 
-        fn get_total_supply(self: @ContractState) -> felt252 {
-            self.total_supply.read()
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).copied().unwrap_or(0)
         }
 
-        fn balance_of(self: @ContractState, account: ContractAddress) -> felt252 {
-            self.balances.read(account)
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.transfer_from_to(from, to, value)
         }
 
-        fn allowance(
-            self: @ContractState, owner: ContractAddress, spender: ContractAddress
-        ) -> felt252 {
-            self.allowances.read((owner, spender))
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value);
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                value,
+            });
+            Ok(())
         }
 
-        fn transfer(ref self: ContractState, recipient: ContractAddress, amount: felt252) {
-            let sender = get_caller_address();
-            self._transfer(sender, recipient, amount);
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance(from, caller);
+            if allowance < value {
+                return Err(Error::InsufficientAllowance)
+            }
+            self.transfer_from_to(from, to, value)?;
+            self.allowances.insert((from, caller), allowance - value);
+            Ok(())
         }
 
-        fn transfer_from(
-            ref self: ContractState,
-            sender: ContractAddress,
-            recipient: ContractAddress,
-            amount: felt252
-        ) {
-            let caller = get_caller_address();
-            self.spend_allowance(sender, caller, amount);
-            self._transfer(sender, recipient, amount);
-        }
-
-        fn approve(ref self: ContractState, spender: ContractAddress, amount: felt252) {
-            let caller = get_caller_address();
-            self.approve_helper(caller, spender, amount);
-        }
-
-        fn increase_allowance(
-            ref self: ContractState, spender: ContractAddress, added_value: felt252
-        ) {
-            let caller = get_caller_address();
-            self
-                .approve_helper(
-                    caller, spender, self.allowances.read((caller, spender)) + added_value
-                );
-        }
-
-        fn decrease_allowance(
-            ref self: ContractState, spender: ContractAddress, subtracted_value: felt252
-        ) {
-            let caller = get_caller_address();
-            self
-                .approve_helper(
-                    caller, spender, self.allowances.read((caller, spender)) - subtracted_value
-                );
-        }
-    }
-
-    #[generate_trait]
-    impl InternalImpl of InternalTrait {
-        fn _transfer(
-            ref self: ContractState,
-            sender: ContractAddress,
-            recipient: ContractAddress,
-            amount: felt252
-        ) {
-            assert(sender.is_non_zero(), Errors::TRANSFER_FROM_ZERO);
-            assert(recipient.is_non_zero(), Errors::TRANSFER_TO_ZERO);
-            self.balances.write(sender, self.balances.read(sender) - amount);
-            self.balances.write(recipient, self.balances.read(recipient) + amount);
-            self.emit(Transfer { from: sender, to: recipient, value: amount });
-        }
-
-        fn spend_allowance(
-            ref self: ContractState,
-            owner: ContractAddress,
-            spender: ContractAddress,
-            amount: felt252
-        ) {
-            let allowance = self.allowances.read((owner, spender));
-            self.allowances.write((owner, spender), allowance - amount);
-        }
-
-        fn approve_helper(
-            ref self: ContractState,
-            owner: ContractAddress,
-            spender: ContractAddress,
-            amount: felt252
-        ) {
-            assert(spender.is_non_zero(), Errors::APPROVE_TO_ZERO);
-            self.allowances.write((owner, spender), amount);
-            self.emit(Approval { owner, spender, value: amount });
-        }
-
-        fn mint(ref self: ContractState, recipient: ContractAddress, amount: felt252) {
-            assert(recipient.is_non_zero(), Errors::MINT_TO_ZERO);
-            let supply = self.total_supply.read() + amount;
-            self.total_supply.write(supply);
-            let balance = self.balances.read(recipient) + amount;
-            self.balances.write(recipient, balance);
-            self
-                .emit(
-                    Event::Transfer(
-                        Transfer {
-                            from: contract_address_const::<0>(), to: recipient, value: amount
-                        }
-                    )
-                );
+        fn transfer_from_to(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance)
+            }
+            self.balances.insert(from, from_balance - value);
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, to_balance + value);
+            self.env().emit_event(Transfer {
+                from: Some(from),
+                to: Some(to),
+                value,
+            });
+            Ok(())
         }
     }
+
 }`;
 
 export default erc20;
